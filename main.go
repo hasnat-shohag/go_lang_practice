44ee016954ab79a733e45d42c4cba19e748d9201
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2024-01-20 16:08:28 +0600

    CRUD operation using Postman by Created three main routes

--- main.go
+++ main.go
@@ -1,15 +1,54 @@
 package main
 
 import (
+	"github.com/labstack/echo/v4"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"net/http"
+	"strconv"
 )
+
+type User struct {
+	gorm.Model
+	Name string
+	Age  int
+}
 
 var DB *gorm.DB
 
+func CreateUsers(c echo.Context) error {
+	user := &User{}
+	if err := c.Bind(user); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+	if err := DB.Create(&user).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusCreated, "User created successfully")
+}
+func GetAllUsers(c echo.Context) error {
+	users := []User{}
+	if err := DB.Find(&users).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, users)
+}
+func DeleteUsers(c echo.Context) error {
+	id := c.Param("id")
+	ID, err := strconv.ParseInt(id, 0, 0)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	user := &User{}
+	if err := DB.Where("id = ?", ID).Delete(&user).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, "user Deleted successfully")
+}
+
 func main() {
 	// New Echo Instance
-	//e := echo.New()
+	e := echo.New()
 
 	// Initiate DB
 	dsn := "root:root@tcp(localhost:3306)/Test?charset=utf8mb4&parseTime=True&loc=Local"
@@ -18,4 +57,11 @@ func main() {
 		panic(err)
 	}
 	DB = d
+	DB.AutoMigrate(&User{})
+
+	e.GET("/users", GetAllUsers)
+	e.POST("/users", CreateUsers)
+	e.DELETE("/users/:id", DeleteUsers)
+
+	e.Logger.Fatal(e.Start(":1111"))
 }
